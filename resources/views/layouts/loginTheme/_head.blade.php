2e67d25245d9b87f2558b38ac603773dfc20a3df
saddam <eduhubweb@gmail.com>
2019-09-16 17:44:52 +0600

    master temleting done with dashboard view

---
+++ resources/views/layouts/loginTheme/_head.blade.php
@@ -0,0 +1,27 @@
+
+
+
+<title>Dashboard Login </title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->
+<link rel="icon" type="image/png" href="{{asset('login1/images/icons/favicon.ico')}}"/>
+<!--===============================================================================================-->
+<link rel="stylesheet" type="text/css" href="{{asset('login1/vendor/bootstrap/css/bootstrap.min.css')}}">
+<!--===============================================================================================-->
+<link rel="stylesheet" type="text/css" href="{{asset('login1/fonts/font-awesome-4.7.0/css/font-awesome.min.css')}}">
+<!--===============================================================================================-->
+<link rel="stylesheet" type="text/css" href="{{asset('login1/fonts/iconic/css/material-design-iconic-font.min.css')}}">
+<!--===============================================================================================-->
+<link rel="stylesheet" type="text/css" href="{{asset('login1/vendor/animate/animate.css')}}">
+<!--===============================================================================================-->
+<link rel="stylesheet" type="text/css" href="{{asset('login1/vendor/css-hamburgers/hamburgers.min.css')}}">
+<!--===============================================================================================-->
+<link rel="stylesheet" type="text/css" href="{{asset('login1/vendor/animsition/css/animsition.min.css')}}">
+<!--===============================================================================================-->
+<link rel="stylesheet" type="text/css" href="{{asset('login1/vendor/select2/select2.min.css')}}">
+<!--===============================================================================================-->
+<link rel="stylesheet" type="text/css" href="{{asset('login1/vendor/daterangepicker/daterangepicker.css')}}">
+<!--===============================================================================================-->
+<link rel="stylesheet" type="text/css" href="{{asset('login1/css/util.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('login1/css/main.css')}}">
---
+++ resources/views/layouts/loginTheme/_scripts.blade.php
@@ -0,0 +1,16 @@
+
+<script src="{{asset('login1/vendor/jquery/jquery-3.2.1.min.js')}}"></script>
+<!--===============================================================================================-->
+<script src="{{asset('login1/vendor/animsition/js/animsition.min.js')}}"></script>
+<!--===============================================================================================-->
+<script src="{{asset('login1/vendor/bootstrap/js/popper.js')}}"></script>
+<script src="{{asset('login1/vendor/bootstrap/js/bootstrap.min.js')}}"></script>
+<!--===============================================================================================-->
+<script src="{{asset('login1/vendor/select2/select2.min.js')}}"></script>
+<!--===============================================================================================-->
+<script src="{{asset('login1/vendor/daterangepicker/moment.min.js')}}"></script>
+<script src="{{asset('login1/vendor/daterangepicker/daterangepicker.js')}}"></script>
+<!--===============================================================================================-->
+<script src="{{asset('login1/vendor/countdowntime/countdowntime.js')}}"></script>
+<!--===============================================================================================-->
+<script src="{{asset('login1/js/main.js')}}"></script>
